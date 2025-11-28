8f554457fad849911da7fd8b0dac23b4e47844bc
Balduc3011 <ducnb3011@gmail.com>
2025-11-28 16:57:40 +0700

    load info and base anim

--- js/script.js
+++ js/script.js
@@ -1,60 +1,102 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (400 < currentScrollPos) {
-    document.getElementById("leftpart").style.opacity = "1";
-    document.getElementById("centerpart").style.opacity = "1";  
-  }
-  if (1200 < currentScrollPos) {
-    document.getElementsByClassName("left2")[0].style.marginLeft = "-20px";
-    document.getElementsByClassName("right2")[0].style.marginLeft = "-200px";
-  }
-  
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("menubar").style.top = "0";
-  } else {
-    document.getElementById("menubar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-/*
-var leftPart = document.getElementById("Reviews");
-var rect = leftPart.getBoundingClientRect();
-alert("Y: " + rect.y);
-var currentScrollPos = window.pageYOffset;
-alert(currentScrollPos);
-*/
+var projectList;
+var iosLink;
+var androidLink;
+var pcLink;
 
-var intro = document.getElementsByClassName("intro")[0];
-intro.volume = 0.1;
-var btn = document.getElementsByClassName("myBtn")[0];
-var introplay = true;
-function playintro() {
-    if (introplay == false) {
-      intro.play();
-      introplay = true;
-      btn.innerHTML = "Pause Intro";
-    } else {
-      intro.pause();
-      introplay = false;
-      btn.innerHTML = "Play Intro";
+function FetchData() {
+  return fetch("https://opensheet.elk.sh/1RY5JCg4WrsETn6vzrRp27dch2V9zgx1kI3xBLnaymWY/ProjectList")
+      .then(res => res.json())
+      .then(list => { 
+        projectList = list;
+        ShowProjectInfo(1);
+      })
+      .catch(err => console.error("Error reading JSON:", err));       
+}
+
+function ShowProjectInfo(id) { 
+  if (!projectList) {
+    console.warn("Data hasn't ready!");
+    return;
+  }
+  projectList.forEach(item => {
+    if(item.id == id) {
+      document.getElementById("id_ProjectName").textContent = item.projectName;
+      document.getElementById("id_ProjectDescription").textContent = item.description;
+      iosLink = item.iosLink;
+      androidLink = item.androidLink;
+      RunShowInfoAnim();
+      RunShowBannerAnim();
     }
+    }
+  );   
+  SetActiveMiniIcon(id);                                   
 }
 
-var imgwidth = document.getElementsByClassName("Gbox")[0].clientWidth;
-var SlideGPage = document.getElementsByClassName("slide")[0];
-var page = document.getElementsByClassName("page");
-var Max = imgwidth * (page.length - 1);
-var Length = 0;
-
-function Next() {
-  if(Length < Max) Length += imgwidth;
-  else Length = 0;
-  SlideGPage.style.marginLeft = '-' + Length + 'px'; 
-}
-function Back() {
-  if (Length == 0) Length = Max;
-  else Length -= imgwidth;
-  SlideGPage.style.marginLeft = '-' + Length + 'px';
+function RunShowInfoAnim() {
+  let element = document.getElementById("character__content");
+  if(element == null)
+      return;
+  element.classList.remove('fade_in_anim');
+  element.classList.add('fade_in_anim');
+  element.addEventListener('animationend', () => {
+    element.classList.remove('fade_in_anim');
+  });
 }
 
+function RunShowBannerAnim() {
+  let element = document.getElementById("character__person");
+  if(element == null)
+      return;
+  element.classList.remove('fade_in_anim');
+  element.classList.remove('character_move_in_anim');
+  element.classList.add('fade_in_anim');
+  element.classList.add('character_move_in_anim');
+  element.addEventListener('animationend', () => {
+    element.classList.remove('fade_in_anim');
+    element.classList.remove('character_move_in_anim');
+  });
+}
+
+function SetActiveMiniIcon(id) {
+  projectList.forEach(item => {
+    let element = document.getElementById("character_page_mini_icon_"+item.id);
+    if(element == null)
+      return;
+    if(item.id == id) {
+      element.classList.add('swiper-slide-thumb-active');
+      element.classList.remove('swiper-slide-active');
+    }
+    else {
+      element.classList.add('swiper-slide-active');
+      element.classList.remove('swiper-slide-thumb-active');
+    }
+    }
+  );   
+}
+
+
+function OpenStoreLink(deviceType) {
+  if(deviceType == DeviceType.IOS) {
+    OpenLink(iosLink,"_blank")
+  }
+  else if(deviceType == DeviceType.Android) {
+    OpenLink(androidLink,"_blank")
+  }
+}
+// Target: _blank, _self
+function OpenLink(link, target) {
+  if(isNullEmptyOrWhitespace(link)) {
+    return;
+  }
+  window.open(link,target)
+}
+
+const DeviceType = Object.freeze({
+  IOS: "ios",
+  Android: "android",
+  Pc: "pc",
+})
+
+function isNullEmptyOrWhitespace(value) {
+  return value === null || (typeof value === 'string' && value.trim() === '') || value == undefined;
+}
